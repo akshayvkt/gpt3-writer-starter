91f562ab3f7c9b32414a8330a74df6a9b690639f
Akshay Chintalapati <64036106+akshayvkt@users.noreply.github.com>
2022-12-15 08:25:52 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -80,13 +80,14 @@ const Home = () => {
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
+          // href="https://buildspace.so/builds/ai-writer"
+          href="https://twitter.com/ch_venkatakshay"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>built by akshay ft. buildspace</p>
           </div>
         </a>
       </div>
